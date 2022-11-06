7246c80a7a3e4e1bd71b7b744c5ebb3f06617a31
TerminalRdK <daltoncuenkcs@gmail.com>
2022-11-06 15:03:05 -0500

    listo

--- Tareas/src/app/components/tareas/tareas.component.ts
+++ Tareas/src/app/components/tareas/tareas.component.ts
@@ -14,6 +14,9 @@ export class TareasComponent implements OnInit {
   ngOnInit(): void {
   }
   agregarTarea(): void {
+    if(this.nombreTarea==''){
+      return;
+    }
     const TAREA: Tarea = {
       nombre: this.nombreTarea,
       estado: false
