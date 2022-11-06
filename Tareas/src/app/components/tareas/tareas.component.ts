7f86d78e5e11b327e14a6e73d3e55abaa21e0798
TerminalRdK <daltoncuenkcs@gmail.com>
2022-11-06 15:01:33 -0500

    terminado

--- Tareas/src/app/components/tareas/tareas.component.ts
+++ Tareas/src/app/components/tareas/tareas.component.ts
@@ -1,3 +1,4 @@
+import { Tarea } from './../../models/Tarea';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tareas.component.css']
 })
 export class TareasComponent implements OnInit {
-
+  listTareas: Tarea[] = []
+  nombreTarea = ''
   constructor() { }
 
   ngOnInit(): void {
   }
+  agregarTarea(): void {
+    const TAREA: Tarea = {
+      nombre: this.nombreTarea,
+      estado: false
+    }
 
+    this.listTareas.push(TAREA);
+
+    this.nombreTarea = ''
+  }
+
+  eliminarTarea(index: number): void {
+    this.listTareas.splice(index, 1);
+  }
+
+  actualizarTarea(tarea: Tarea, index: number): void {
+    this.listTareas[index].estado = !tarea.estado;
+  }
 }
