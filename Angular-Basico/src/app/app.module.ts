99f9d9c45d9a76fe03d86d0fa6fdbeeb797cda2a
TerminalRdK <daltoncuenkcs@gmail.com>
2022-11-26 18:13:59 -0500

    angular basico

--- Angular-Basico/src/app/app.module.ts
+++ Angular-Basico/src/app/app.module.ts
@@ -1,14 +1,20 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BasicoComponent } from './components/basico/basico.component';
+import { DirectivasComponent } from './components/directivas/directivas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicoComponent,
+    DirectivasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
